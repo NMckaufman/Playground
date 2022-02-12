39889d88a9324ca5d3bca80f8bcf6bc69756702d
NMckaufman <nisa_alise@verizon.net>
2022-02-12 15:29:12 -0800

    Playground(attempt 1)

---
+++ pservin.py
@@ -0,0 +1,22 @@
+from distutils.log import debug
+from sys import builtin_module_names
+from flask import Flask, render_template
+play= Flask(__name__)
+
+@play.route('/play/<int:num>/<string:color>')
+def seven_squares(num,color):
+    return render_template('index.html',num=num, color=color)
+
+@play.route('/play/<int:num>')
+def blue_squares(num):
+    return render_template('index.html',num=num, color="blue")
+
+@play.route('/play')
+def squares():
+    return render_template('index.html',num=3, color="blue")
+
+
+
+
+if __name__=="__main__":
+    play.run(debug=True)
